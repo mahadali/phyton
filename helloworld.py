e38455deec8d381c7171eb34e1836d7d9549bd5c
smaali1 <mahad.ali2@gmail.com>
2017-10-24 19:44:49 +0500

    Added hello word file

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+print('Hello World');
+
+
+# message = ("Hello Python123 world");
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+bicycles[2] = "shimano"
+bicycles.append('cycle')
+mylist = "My cycle list" + " " + bicycles[0]
+
+print(bicycles)
+
+motorcycles = []
+
+motorcycles.append('superpower');
+motorcycles.append('honda');
+
+motorcycles[0] = 'honda';
+
+del motorcycles[1]
+print(motorcycles)
+
+# print(bicycles[-1].title())
+# print(message))
